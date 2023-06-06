f4edcc704a30bdbbd7fa10dc5009e1b4ff0ac0a5
Lileczka <liliana.guderska@gmail.com>
2023-06-06 16:05:01 +0200

    upgreade version

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+  //const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,97 +15,96 @@ class MyApp extends StatelessWidget {
           backgroundColor: Color.fromRGBO(11, 124, 181, 1),
           title: Text('Carte de visite'),
         ),
-        backgroundColor:  Color.fromRGBO(114, 164, 189, 1),
+        backgroundColor: Color.fromRGBO(114, 164, 189, 1),
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-               SizedBox(height: 120.0), 
-          CircleAvatar(
-            radius: 70.0,
-            backgroundColor: Colors.transparent,
-            child: ClipOval(
-              child: Image.asset(
-                'images/portretCV.png',
-                fit: BoxFit.fill,
-                // Ajuste l'image pour couvrir tout l'espace
-                width: 140.0, // Largeur de l'image
-                height: 140.0, // Hauteur de l'image
-              ),
-            ),
-          ),
-          Text(
-            'Lilianna Guderska',
-            style: TextStyle(
-              fontFamily: 'Pacifico',
-              color: Colors.white,
-              fontSize: 35.0,
-            ),
-          ),
-          SizedBox(
-            height: 20.0,
-            width: 300.0,
-            child: Divider(
-              color: Colors.white,
-              thickness: 1.0, 
-              ),
-            ),
-          Text(
-            'Developpeuse web',
-            style: TextStyle(
-              fontFamily: 'Roboto',
-              fontSize: 24.0,
-              color: Color.fromARGB(255, 3, 6, 19),
-            ),
-          ),
-
-          Card(
-            color: Colors.white,
-            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-           child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
-                  Icon(
-                    Icons.phone_iphone,
-                    size: 30.0,
-                    color: Color.fromRGBO(11, 124, 181, 1),
+              SizedBox(height: 120.0),
+              CircleAvatar(
+                radius: 70.0,
+                backgroundColor: Colors.transparent,
+                child: ClipOval(
+                  child: Image.asset(
+                    'images/portretCV.png',
+                    fit: BoxFit.fill,
+                    // Ajuste l'image pour couvrir tout l'espace
+                    width: 140.0, // Largeur de l'image
+                    height: 140.0, // Hauteur de l'image
                   ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    '+336 88 84 64 92',
-                    style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                  
-                ],
-              ),
-            ),
-          ),
-          Card(
-            color: Colors.white,
-            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-            child: ListTile(
-              leading: Icon(
-                  Icons.email,
-                  size: 30.0,
-                  color: Color.fromRGBO(11, 124, 181, 1),
                 ),
-              title: Text(
-                  'lilianna.guderska@renault.com',
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0,),
               ),
-              )
-          )
-        ])),
+              Text(
+                'Lilianna Guderska',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  color: Colors.white,
+                  fontSize: 35.0,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 300.0,
+                child: Divider(
+                  color: Colors.white,
+                  thickness: 1.0,
+                ),
+              ),
+              Text(
+                'Developpeuse web',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 24.0,
+                  color: Color.fromARGB(255, 3, 6, 19),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone_iphone,
+                        size: 30.0,
+                        color: Color.fromRGBO(11, 124, 181, 1),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+336 88 84 64 92',
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      size: 30.0,
+                      color: Color.fromRGBO(11, 124, 181, 1),
+                    ),
+                    title: Text(
+                      'lilianna.guderska@renault.com',
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ))
+            ])),
       ),
     );
   }

b27702fcd122ee77dd7873b0d32c67c19c2da926
Lileczka <liliana.guderska@gmail.com>
2023-07-31 11:42:40 +0200

    liens vers socialmedia

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_social_button/flutter_social_button.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  //const MyApp({Key key}) : super(key: key);
+  final String _urlGit = 'https://github.com/Widgetanna?tab=repositories';
+  final String _urlFace = 'https://www.facebook.com/public/Lilianna-Guderska';
 
-  @override
+  Future<void> _launchUrl(String url) async {
+    // Convertir la chaîne de caractères `url` en un objet Uri
+    Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      // Ouvre le lien
+      await launchUrl(uri);
+    } else {
+      throw Exception('Could not launch $url');
+    }
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -17,9 +30,9 @@ class MyApp extends StatelessWidget {
         ),
         backgroundColor: Color.fromRGBO(114, 164, 189, 1),
         body: SafeArea(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
               SizedBox(height: 120.0),
               CircleAvatar(
                 radius: 70.0,
@@ -28,9 +41,8 @@ class MyApp extends StatelessWidget {
                   child: Image.asset(
                     'images/portretCV.png',
                     fit: BoxFit.fill,
-                    // Ajuste l'image pour couvrir tout l'espace
-                    width: 140.0, // Largeur de l'image
-                    height: 140.0, // Hauteur de l'image
+                    width: 140.0,
+                    height: 140.0,
                   ),
                 ),
               ),
@@ -42,8 +54,17 @@ class MyApp extends StatelessWidget {
                   fontSize: 35.0,
                 ),
               ),
+              Text(
+                'WIDGETANNA',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  color: Colors.white,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.w100,
+                ),
+              ),
               SizedBox(
-                height: 20.0,
+                height: 35.0,
                 width: 300.0,
                 child: Divider(
                   color: Colors.white,
@@ -57,6 +78,9 @@ class MyApp extends StatelessWidget {
                   fontSize: 24.0,
                   color: Color.fromARGB(255, 3, 6, 19),
                 ),
+              ),
+              SizedBox(
+                height: 8.0,
               ),
               Card(
                 color: Colors.white,
@@ -86,25 +110,50 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               Card(
-                  color: Colors.white,
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.email,
-                      size: 30.0,
-                      color: Color.fromRGBO(11, 124, 181, 1),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 30.0,
+                    color: Color.fromRGBO(11, 124, 181, 1),
+                  ),
+                  title: Text(
+                    'lilianna.guderska@renault.com',
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
                     ),
-                    title: Text(
-                      'lilianna.guderska@renault.com',
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ))
-            ])),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FlutterSocialButton(
+                    onTap: () {
+                      _launchUrl(_urlGit);
+                    },
+                    buttonType: ButtonType.github,
+                    mini: true,
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  FlutterSocialButton(
+                    onTap: () {
+                      _launchUrl(_urlFace);
+                    },
+                    buttonType: ButtonType.facebook,
+                    mini: true,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
